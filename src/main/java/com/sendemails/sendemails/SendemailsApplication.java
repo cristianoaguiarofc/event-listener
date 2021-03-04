bd69178c9093c89813c98f3a60ef161b80e60d17
Cristiano Andrade de Aguiar <cristianoandrade.deaguiar@gmail.com>
2021-03-04 19:35:22 -0300

    Commit inicial

---
+++ src/main/java/com/sendemails/sendemails/SendemailsApplication.java
@@ -0,0 +1,13 @@
+package com.sendemails.sendemails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendemailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendemailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sendemails/sendemails/SendemailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sendemails.sendemails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendemailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
